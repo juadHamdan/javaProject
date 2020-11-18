da93de93029ea05d0d0719e6293351d25eef39bb
Rami joad Hamdan <joad.hamdan@gmail.com>
2020-11-18 19:56:53 +0200

    first commit

--- console/src/application/BMSMain.java
+++ console/src/application/BMSMain.java
@@ -3,6 +3,13 @@ package application;
 import engine.BMSEngine;
 import engine.Engine;
 
+/*
+* Aharon:
+* Boat, registration, auth
+* Joad:
+* Rower, activity, validation, menu.
+* */
+
 public class BMSMain {
     private BMSEngine engine;
 
